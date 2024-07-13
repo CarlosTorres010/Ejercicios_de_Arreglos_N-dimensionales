bc1c6b5bb8265d689a1df664c5a10054d65a3247
Carlostorres010 <ctorresv@unjbg.edu.pe>
2024-07-12 22:00:11 -0500

    Se incluyo el 4 ejercicio de arreglos dimensionales

---
+++ 4-Ejercicio.cpp
@@ -0,0 +1,50 @@
+/*
+4.
+Intercambiar Filas
+Desarrolle un programa que intercambie dos filas específicas de una matriz
+de 4x4 y muestre la matriz resultante.
+*/	
+#include <iostream>
+using namespace std;
+
+const int N = 4; 
+
+void intercambiarFilas(int matriz[4][4], int fila1, int fila2) {
+    if (fila1 >= 0 && fila1 < 4 && fila2 >= 0 && fila2 < 4) {
+        for (int j = 0; j < 4; j++) {
+            int temp = matriz[fila1][j];
+            matriz[fila1][j] = matriz[fila2][j];
+            matriz[fila2][j] = temp;
+        }
+    } else {
+        cout << "Upps las filas especificadas estan fuera del rango de la matriz." << endl;
+    }
+}
+
+void mostrarMatriz(int matriz[4][4]) {
+    cout << "Matriz resultante:" << endl;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int matriz[4][4] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
+    
+    int filaIntercambiar1 = 0; 
+    int filaIntercambiar2 = 3; 
+
+    cout << "Matriz original:" << endl;
+    mostrarMatriz(matriz);
+
+    intercambiarFilas(matriz, filaIntercambiar1, filaIntercambiar2);
+	mostrarMatriz(matriz);
+	return 0;
+}
+
